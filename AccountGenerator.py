a0dc776ee5a6519c69468434db0a5ef6a4d46b2a
m-Mateja <93741754+m-Mateja@users.noreply.github.com>
2021-11-07 11:53:23 -0500

    Add files via upload

---
+++ AccountGenerator.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import random
+
+df = pd.read_excel(r'C:\Users\mateya\Desktop\InstagramBotInfo.xlsx')
+'print(df.to_string())'
+
+row = 0
+
+while (1):
+    
+    cellFName = df.iloc[row,0]
+    cellLName = df.iloc[row,1]
+    
+    if cellFName == 'END':
+        break
+    
+    'Password Generator---------------------------------------------------------'
+    lower = 'abcdefghijklmnopqrstuvwxyz'
+    upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    num = '0123456789'
+    symbol = '!@#$%^&*()_+'
+    
+    combinePass = lower+upper+num+symbol
+    length = 10
+    password = "".join(random.sample(combinePass,length))
+    print (password)
+    df.iloc[row,3] = password
+    
+    'Email Generator-------------------------------------------------------------'
+    email = cellFName+cellLName+"".join(random.sample(num,5))
+    print (email)
+    df.iloc[row,2] = email
+    
+    'Place Email into Excel------------------------------------------------------'
+    writer = pd.ExcelWriter('UsernamePass.xlsx')
+    df.to_excel(writer,'new_sheet')
+    writer.save()
+    row = row + 1
